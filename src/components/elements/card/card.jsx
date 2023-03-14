90b21731a1efb19cbabfe8435c20e4f0501af5cc
mussyaroslav <kiborg1101@gmail.com>
2023-03-14 15:31:18 +0500

    Фикс скролла

--- src/components/elements/card/card.jsx
+++ src/components/elements/card/card.jsx
@@ -1,13 +1,13 @@
 import './card.css';
 
-function Card({title, description, price, time, image}) {
+function Card({title, description, price, quantity, image}) {
   return (
     <div className='product__card'>
       <div className='product__img__div'>
         <img alt='' className='product__img' src={image} />
       </div>
       <div className='product__title'>
-        <p>{title}</p>
+        <a href='#'>{title}</a>
       </div>
       <div className='product__description'>
         <p>{description}</p>
@@ -16,14 +16,14 @@ function Card({title, description, price, time, image}) {
         <div className='price__div'>
           <p className='price'>{price}</p>
           <p className='slash'>&nbsp; / &nbsp;</p>
-          <p className='time'>{time}</p>     
+          <p className='time'>{quantity}</p>     
         </div>
-        <div className='btn__plus'>
+        <button className='btn__plus'>
           <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
             <path d="M7 1.28564V12.3571" stroke="white" stroke-width="2" stroke-linecap="round"/>
             <path d="M12.3569 6.82135L1.28551 6.82135" stroke="white" stroke-width="2" stroke-linecap="round"/>
           </svg>
-        </div>
+        </button>
       </div>
     </div>
   )
